ec8383ef3489fef7c15914d9b1120b8e87c03b00
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-04-08 15:59:34 -0400

    Update ItemDBS2.php

--- SamsBakery/ItemDBS2.php
+++ SamsBakery/ItemDBS2.php
@@ -11,7 +11,7 @@ $db= new PDO($connection_string, $dbuser, $dbpass);
 echo "should have connected";
 $stmt = $db->prepare("CREATE TABLE IF NOT EXISTS
   `tblproduct` (
-    `id` int auto_increment NOT NULL,
+    `id` int auto_increment not null,
     `name` varchar(255) NOT NULL,
     `code` varchar(255) NOT NULL,
     `image` text NOT NULL,
@@ -25,22 +25,4 @@ catch(Exception $e){
 echo $e->getMessage();
 exit("It didn't work");
 }
-
-
-try{
-$db= new PDO($connection_string, $dbuser, $dbpass);
-echo "should have connected";
-$stmt = $db->prepare("INSERT INTO `tblproduct`
-  (`name`, `code`, `image`, `price`) VALUES
-('FinePix Pro2 3D Camera', '3DcAM01', 'product-images/camera.jpg', 1500.00),
-('EXP Portable Hard Drive', 'USB02', 'product-images/external-hard-drive.jpg', 800.00),
-('Luxury Ultra thin Wrist Watch', 'wristWear03', 'product-images/watch.jpg', 300.00),
-('XP 1155 Intel Core Laptop', 'LPN45', 'product-images/laptop.jpg', 800.00)");
-$stmt->execute();
-}
-catch(Exception $e){
-echo $e->getMessage();
-exit("It didn't work");
-}
-
 ?>
