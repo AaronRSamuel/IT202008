764f9d961692456bb5be54b0b35a71db4323e8a0
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-04-14 19:31:27 -0400

    Create NewItem.php

---
+++ SamsBakery/NewItem.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+  <div>
+    <center><form name="new_item" id="new_item" method="POST">
+      <label for="name">Name: </label>
+      <input type="text" id="name" name="name" placeholder="Enter item name"/>
+      <label for="comment">pricer: </label>
+      <input type="int" id="price" name="price" placeholder="Enter price"/>
+      <label for="photo">photo URL: </label>
+      <input type="text" id="photo" name="photo" placeholder="Enter photo URL"/>
+      <label for="listLoc">location: </label>
+      <input type="int" id="listLoc" name="listLoc" placeholder="Enter list location(number)"/>
+      <input type="submit" value="submit"/>
+    </form></center>
+  </div>
+</body>
+</html>
+
+<?php
+require ("config.php");
+echo "DBUser: " . $dbuser;
+echo "\n\r";
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+
+try{
+$db= new PDO($connection_string, $dbuser, $dbpass);
+echo "should have connected";
+$stmt = $db->prepare("INSERT INTO
+  `tblproduct` (name, code, image, price)
+  VALUES (:name, :code, :image, :price) ");
+  $name = $_POST['name'];
+  $code = $_POST['listLoc'];
+  $image = $_POST['photo'];
+  $price = $_POST['price'];
+  $params = array(":name"=> $name, ":code"=> $code,":photo"=> $photo,":image"=> $image);
+  $stmt->execute($params);
+  echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
+}
+catch(Exception $e){
+echo $e->getMessage();
+exit("It didn't work");
+}
+ ?>
