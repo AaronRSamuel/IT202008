d222655f3671316beef7c3fbc2d69d4e52a3de94
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-04-14 15:32:11 -0400

    Update custom_order.php

--- SamsBakery/custom_order.php
+++ SamsBakery/custom_order.php
@@ -17,15 +17,14 @@ session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-if(        isset($_POST['phone'])
-        && isset($_POST['comment'])){
+try{
+if(isset($_POST['phone']) && isset($_POST['comment'])){
 $phone = $_POST['phone'];
 $comment = $_POST['comment'];
 $user_Id = $_SESSION['id'];
 
 require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-try{
 $db = new PDO($connection_string, $dbuser, $dbpass);
 
 $stmt = $db->prepare(" INSERT INTO `Orders`
