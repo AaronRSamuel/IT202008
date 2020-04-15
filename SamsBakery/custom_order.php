8b835f63eb9d275cd160b3d83123f8f1ceafffad
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-04-14 20:19:54 -0400

    Update custom_order.php

--- SamsBakery/custom_order.php
+++ SamsBakery/custom_order.php
@@ -17,25 +17,27 @@ session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-try{
+
 if(isset($_POST['phone']) && isset($_POST['comment'])){
-$phone = $_POST['phone'];
-$comment = $_POST['comment'];
-$user_Id = $_SESSION['id'];
+  try{
+    $phone = $_POST['phone'];
+    $comment = $_POST['comment'];
+    $user_Id = $_SESSION['id'];
 
-require("config.php");
-$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-$db = new PDO($connection_string, $dbuser, $dbpass);
+    require("config.php");
+    $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+    $db = new PDO($connection_string, $dbuser, $dbpass);
 
-$stmt = $db->prepare(" INSERT INTO `Orders`
-      (user_id, phone_number, item_id, comment) VALUES
-      (:user_ID, :phone, :item_id, :comment)");
-$params = array(":user_ID"=> $user_Id, ":phone"=> $phone, ":item_id" => 33, ":comment" => $comment);
-$stmt->execute($params);
-}
-}
-catch(Exception $e){
-  echo $e->getMessage();
-  exit();
+    $stmt = $db->prepare(" INSERT INTO `Orders`
+          (user_id, phone_number, item_id, comment) VALUES
+          (:user_ID, :phone, :item_id, :comment)");
+    $params = array(":user_ID"=> $user_Id, ":phone"=> $phone, ":item_id" => 33, ":comment" => $comment);
+    $stmt->execute($params);
+    echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
+  }
+  catch(Exception $e){
+    echo $e->getMessage();
+    exit();
+  }
 }
 ?>
