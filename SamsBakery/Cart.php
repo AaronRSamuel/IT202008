f5621f50538704c5f941a70acbf8ee5898e22df3
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-05-08 14:05:02 -0400

    Update Cart.php

--- SamsBakery/Cart.php
+++ SamsBakery/Cart.php
@@ -1,4 +1,20 @@
-<!DOCTYPE html>
+<?php
+session_start();
+echo $_SESSION['id'];
+require ("config.php");
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+try{
+  $db= new PDO($connection_string, $dbuser, $dbpass);
+  echo "should have connected";
+  $sql = $db->prepare("SELECT item_name, item_price, from Orders where user_id= $_SESSION['id']");
+  $sql->execute();
+}
+catch(Exception $e){
+echo $e->getMessage();
+exit("It didn't work");
+}
+?>
 <html lang="en">
   <head>
     <title></title>
@@ -10,19 +26,25 @@
   </style>
   <body>
     <table id = "Cart">
+      <thread>
       <tr>
         <th>Item</th>
         <th>Price</th>
       </tr>
+    </thread>
+    <tbody>
+      <?php while( $row = $sql->fetch()) : ?>
+      <tr>
+          <td><?php echo $row['item_name']; ?></td>
+          <td><?php echo $row['item_price']; ?></td>
+      </tr>
+      <?php endwhile ?>
+    </tbody>
     </table>
   </body>
-  <script>
-    var table = document.getElementById("Cart");
-
-  </script>
 </html>
 
 <?php
   session_start();
-  
+
 ?>
