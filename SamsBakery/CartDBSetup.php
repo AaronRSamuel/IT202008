f3cdcba87a74ff1616f0849118e21410023f42fb
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-05-08 13:18:45 -0400

    Create CartDBSetup.php

---
+++ SamsBakery/CartDBSetup.php
@@ -0,0 +1,25 @@
+<?php
+	require ("config.php");
+	echo "DBUser: " . $dbuser;
+	echo "\n\r";
+
+	$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+
+	try{
+	$db= new PDO($connection_string, $dbuser, $dbpass);
+	echo "should have connected";
+	$stmt = $db->prepare("CREATE TABLE IF NOT EXISTS
+			 `Cart` (
+				 	`id` int auto_increment not null,
+          `user_id` int not null,
+          `item_id` int not null,
+				PRIMARY KEY (`id`)
+				) CHARACTER SET utf8 COLLATE utf8_general_ci"
+			);
+	$stmt->execute();
+	}
+	catch(Exception $e){
+	echo $e->getMessage();
+	exit("It didn't work");
+	}
+?>
