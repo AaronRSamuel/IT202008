18665d82d4b5277672d42abacd0bc44b42fddea9
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-05-11 15:36:13 -0400

    Update profile.php

--- SamsBakery/profile.php
+++ SamsBakery/profile.php
@@ -76,8 +76,8 @@ if(        isset($_POST['password'])
               $db = new PDO($connection_string, $dbuser, $dbpass);
               $stmt = $db->prepare("UPDATE `Coustomers`
                       SET `password` = :password
-                      WHERE `id`= $id ");
-      $params = array(":password"=> $pass);
+                      WHERE `id`= :id ");
+      $params = array(":password"=> $pass, ":id"=> $id);
       $stmt->execute($params);
       echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
       }
