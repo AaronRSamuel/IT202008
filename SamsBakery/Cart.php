39cd6f73e203e7945c1f0c103890f083e1d583d2
aaronsNJIT <60156202+aaronsNJIT@users.noreply.github.com>
2020-05-12 21:31:51 -0400

    Update Cart.php

--- SamsBakery/Cart.php
+++ SamsBakery/Cart.php
@@ -12,9 +12,8 @@ else{
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $db= new PDO($connection_string, $dbuser, $dbpass);
 try{
-  $query = "SELECT item_name, item_id, item_price from Cart WHERE user_id = 3";
-  $sql = $db->prepare($query);
-  $sql->execute();
+  $sql = $db->prepare("SELECT item_name, item_id, item_price from Cart WHERE user_id = :id");
+  $sql->execute(array(":id"=>$id));
 }
 catch(Exception $e){
 echo $e->getMessage();
